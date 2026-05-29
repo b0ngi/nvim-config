06f217a9bbe36401cdc84a11516b568cee340e20
fabian schaub <fabi.schaub@proton.me>
2026-05-30 01:10:25 +0200

    disabled neovide animations

--- lua/init.lua
+++ lua/init.lua
@@ -234,4 +234,11 @@ require("dapui").setup()
 
 if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_cursor_animation_length = 0.00
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
+    vim.g.neovide_scroll_animation_far_lines = 0
+    vim.g.neovide_scroll_animation_length = 0.00
 end
